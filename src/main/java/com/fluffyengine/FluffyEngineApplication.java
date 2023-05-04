248b615bd6af36e53bdc4e2898cefd21e8829b51
Mykola Kolchenko <mykola.kolchenko@servian.com>
2023-05-04 14:49:25 +1000

    This adds SBOM generation capability

--- src/main/java/com/fluffyengine/FluffyEngineApplication.java
+++ src/main/java/com/fluffyengine/FluffyEngineApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FluffyEngineApplication {
 
 	public static void main(String[] args) {
+	    String Username;
+        String Password;
+        Password = "admin";
+        Username = "admin";
 		SpringApplication.run(FluffyEngineApplication.class, args);
 	}
 
